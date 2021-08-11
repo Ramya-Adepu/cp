ca06e9c1798ee04cdfd89b55a108fad4fe64f6c0
adepu <adepu.ramya@gmail.com>
2021-08-11 20:53:12 +0530

    submitted.

--- 05-getinrange-Python/getinrange.py
+++ 05-getinrange-Python/getinrange.py
@@ -6,4 +6,9 @@
 
 def fun_getinrange(x, bound1, bound2):
 	# your code goes here
-	return -1
+	if(x<=bound1 and x<=bound2):
+		return min(bound1, bound2)
+	elif(x>=bound2 and x>=bound1):
+		return max(bound2, bound1)
+	else:
+		return x 
