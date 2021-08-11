3e617818499d0354da35ca6448f3fa4b5e73e6d6
adepu <adepu.ramya@gmail.com>
2021-08-11 21:37:11 +0530

    completed.

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -7,5 +7,32 @@
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
+	flag=False
+	if(n<0):
+		n=abs(n)
+		flag=True
+	cnt=0
+	s=0
+	i=0
+	# print("r", "n", "s", "cnt", "i")
+	while(n>0):
+		r=n%10
+		n=n//10
+		if(cnt==k):
+			# print(d*(10**i))
+			s+=d*(10**i)
+		else:
+			s+=r*(10**i)
+		# print(r, n, s, cnt, i)
+		i+=1
+		cnt+=1
+	while(cnt<=k):
+		if(cnt==k):
+			s+=d*(10**i)
+		i+=1
+		cnt+=1
+	if(flag):
+		return -1*s
+	return (s)
 
+print(fun_set_kth_digit(468, 3, 1))
