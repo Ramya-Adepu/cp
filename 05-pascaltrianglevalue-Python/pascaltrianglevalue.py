41c95e6c3052171790f1487a5720d3b57de5d9c8
adepu <adepu.ramya@gmail.com>
2021-08-11 22:28:05 +0530

    pascals triangle completed

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -5,8 +5,29 @@
 # each row starts at column 0. If either row or col 
 # are not legal values, return None, instead of crashing. 
 
-
-
-
 def fun_pascaltrianglevalue(row, col):
-	return 1
+	l=[1,1]
+	cnt=1
+	if(col>row+1):
+		return 0
+	else:
+		while(cnt<row):
+			m=[]
+			m.append(l[0])
+			i=0
+			n=len(l)
+			while(i<n-1):
+				m.append(l[i]+l[i+1])
+				i+=1
+			m.append(l[n-1])
+			print(m, cnt)
+			l=m
+			cnt+=1
+		return l[col]
+
+# print(fun_pascaltrianglevalue(7, 9))
+
+    # (1, 1, 1), (3, 5, 0),
+    # (3, 1, 3), (5, 2, 10),
+    # (6,3,20), (6,2,15),
+    # (7, 4, 35), (7, 9, 0)
