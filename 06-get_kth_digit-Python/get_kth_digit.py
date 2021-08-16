7e05e8eaf3176bcfb6ad2ae4d1e451f9bb027afa
adepu <adepu.ramya@gmail.com>
2021-08-16 22:24:12 +0530

    completed

--- 06-get_kth_digit-Python/get_kth_digit.py
+++ 06-get_kth_digit-Python/get_kth_digit.py
@@ -6,4 +6,18 @@
 
 
 def fun_get_kth_digit(digit, k):
-	return 0
+	n=abs(digit)
+	i=0
+	while(n>0):
+		r=n%10
+		if(i==k):
+			return r
+		i+=1
+		n=n//10
+	if(i<=k):
+		return 0
+	
+print(fun_get_kth_digit(-780, 4))
+    # (789,0,9), (789,1,8),
+    # (789,2,7), (789,3,0),
+    # (-789,0,9), (-780,4,0),
