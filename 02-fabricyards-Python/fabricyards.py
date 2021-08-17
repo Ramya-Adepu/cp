fb961a3cb45aebf3d4c47f1773ce0b12a36e0733
adepu <adepu.ramya@gmail.com>
2021-08-17 19:59:49 +0530

    completed

--- 02-fabricyards-Python/fabricyards.py
+++ 02-fabricyards-Python/fabricyards.py
@@ -11,11 +11,23 @@
 # fabric that must be purchased (as purchases must be in whole yards).
 # Hint: you may want to use fabricyards, which you just wrote!
 
-
+# (0,0),(1,1),(35,1),(36,1),(37,2),(38,2),(72,2),(73,3)
+import math
 def fabricyards(inches):
 	# Your code goes here...
-	return 1
+	# if(inches==0):
+	# 	return 0
+	# else:
+	c=inches/36
+	d=math.ceil(c)
+	return d
 
+    # (0,0),(1,35),(35,1),(36,0),(37,35),(38,34),(72,0),(73,35)
 def fabricexcess(inches):
 	# Your code goes here...
-	return 1
+	c=inches/36
+	d=math.ceil(c)
+	r=((d*36-inches))
+	return r
+
+print(fabricexcess(73))
