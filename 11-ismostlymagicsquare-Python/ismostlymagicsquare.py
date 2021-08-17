4518820cfc6628e837baf0cf1d119af73b6167f5
adepu <adepu.ramya@gmail.com>
2021-08-17 21:13:27 +0530

    Completed

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -15,4 +15,37 @@
 
 def ismostlymagicsquare(a):
 	# Your code goes here
-	pass
+	colsum=[]
+	rowsum=[]
+	dlsum=0
+	drsum=0
+	for i in range(len(a)):
+		row=0
+		col=0
+		for j in range(len(a[0])):
+			if(i==j):
+				dlsum+=a[i][j]
+				col+=a[j][i]
+				row+=a[i][j]
+			if(len(a)-1-i==len(a[0])-1-j):
+				drsum+=a[len(a)-1-i][len(a[0])-1-j]
+				# col+=a[j][i]
+				# row+=a[i][j]
+			else:
+				col+=a[j][i]
+				row+=a[i][j]
+		colsum.append(col)
+		rowsum.append(row)
+	if(dlsum!=drsum):
+		return False
+	elif(len(set(rowsum))!=1):
+		return False
+	elif(len(set(colsum))!=1):
+		return False
+	else:
+		return True
+ismostlymagicsquare([[16, 3, 2, 13], [5, 10, 11, 8], [9, 6, 7, 12],[4, 15, 14, 1]])
+
+	# ([[2, 7, 6], [9, 5, 1], [4, 3, 8]], True),
+	# ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], False),
+	# ([[16, 3, 2, 13], [5, 10, 11, 8], [9, 6, 7, 12],[4, 15, 14, 1]], True)
