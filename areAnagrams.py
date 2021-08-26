e26afcb72083e80d4f1e2e006f135eeb4e9375b0
adepu <adepu.ramya@gmail.com>
2021-08-26 22:45:36 +0530

    Submitted.

--- areAnagrams.py
+++ areAnagrams.py
@@ -12,6 +12,14 @@
 
 def areAnagrams(s1, s2):
     # Your code goes here...
-    pass
+    s1=s1.lower()
+    s2=s2.lower()
+    if len(s1) != len(s2):
+        return False
+    else:
+        for ch in s1:
+            if s1.count(ch) != s2.count(ch):
+                return False
+            return True
 
 # write your test cases here...
