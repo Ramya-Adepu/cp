cd944b20357662868740947ad85aafa474d57dde
adepu <adepu.ramya@gmail.com>
2021-08-26 22:36:25 +0530

    Submitted

--- SayHi.py
+++ SayHi.py
@@ -4,5 +4,17 @@
 # sayHi(d1) # Hi, my name is Dot and I am 4 years old!
 # sayHi(d2) # Hi, my name is Elf and I am 3 years old!
 
-class SayHi(Object):
-    pass
+class SayHi(object):
+   def __init__(self, name, age):
+       self.name = name
+       self.age = age
+  
+   def sayHi1(details):
+       print(f'Hi, my name is {details.name} and I am {details.age} years old!')
+ 
+d1 = SayHi('Dolly', 4)
+d2 = SayHi('pinky', 3)
+ 
+d1.sayHi1()
+d2.sayHi1()
+
