f49ecc38b5fec15f83db71598b27b4abe5cae78e
adepu <adepu.ramya@gmail.com>
2021-08-27 21:10:30 +0530

    Completed

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -6,4 +6,35 @@
 # as does longestDigitRun(-677886).
 def longestdigitrun(n):
 	# Your code goes here
-	pass
+    n=abs(n)
+    count=1
+    max_count=0
+    a=12
+    b=0
+    if(n==0):
+        return n
+    if(n<9):
+        return n
+    while(n>0):
+        r=n%10
+        if(r!=a):
+            count=1
+        n=n//10
+        if(a==r):
+            count=count+1
+        a=r
+        if(count>max_count):
+            max_count=count
+            b=a
+        if(max_count==count):
+            if(b>a):
+                b=a
+    return b
+    
+	# (117773732, 7), 
+    # (-677886, 7), 
+    # (112233455567, 5), 
+    # (44332211, 1), 
+    # (4422113355, 1),
+    # (12345, 1), 
+    # (123330001, 0)
