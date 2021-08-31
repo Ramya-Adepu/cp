7b01f9f0ded995c01b90bde69b0b929ccb02818c
adepu <adepu.ramya@gmail.com>
2021-08-31 23:37:06 +0530

    Completed

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -4,5 +4,34 @@
 
 
 
+def prime(n):
+	if n < 2:
+		return False
+	for i in range(2, n):
+		if n % i == 0:
+			return False
+	return True
+
+def happy(n):
+	while(True):
+		if n == 1:
+			return True
+		elif n == 4:
+			return False
+		else:
+			sum = 0
+			while(n > 0):
+				sum = sum + (n % 10) ** 2
+				n = n // 10
+			n = sum
+			
 def fun_nth_happy_prime(n):
-	return 0
+	num = -1
+	res = 2
+	while(True):
+		if (prime(res) and happy(res)):
+			num = num + 1
+		if num == n:
+			break
+		res = res + 1
+	return res
