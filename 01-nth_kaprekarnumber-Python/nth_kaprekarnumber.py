c86fbbbbd93cedf165c96a7e42f187e0c4f81712
adepu <adepu.ramya@gmail.com>
2021-08-31 23:46:12 +0530

    Submitted

--- 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
+++ 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
@@ -8,6 +8,33 @@
 
 
 import math
+def kaprekar(n):
+    if n==1:
+        return True
+    sq = n*n
+    countd = 0
+    while not sq == 0 :
+        countd = countd + 1
+        sq = sq // 10
+    sq = n*n 
+    d = 0
+    while(d < countd):
+        d = d + 1
+        eq_parts = int(math.pow(10,d))
+        if eq_parts == n :
+            continue
+        sum = (sq//eq_parts) + (sq % eq_parts)
+        if sum == n :
+            return True
+    return False
 
 def fun_nth_kaprekarnumber(n):
-    return 1;
+    l = []
+    i = 1
+    c = 0
+    while(c<=n):
+        if(kaprekar(i)==True):
+            c = c + 1
+            l.append(i)
+        i = i+1
+    return l[n]
