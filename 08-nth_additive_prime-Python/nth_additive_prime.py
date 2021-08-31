0bc9404269eb800ee8b6f1a908e6b288c08d9ded
adepu <adepu.ramya@gmail.com>
2021-08-31 23:42:50 +0530

    Completed

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -6,5 +6,30 @@
 
 
 
+def sum(n):
+	sum = 0
+	while(n > 0):
+		sum = sum + n % 10
+		n = n // 10
+	return sum
+def prime(n):
+	if(n < 2):
+		return False
+	for i in range(2,n):
+		if n % i == 0:
+			return False
+	return True
+
 def fun_nth_additive_prime(n):
-	return 1
+	p = 2
+	c = -1
+	res = 0
+	while(True):
+		s = sum(p)
+		if(prime(p) and prime(s)):
+			c = c + 1
+			res = p
+		p = p + 1
+		if(c >= n):
+			break
+	return res
