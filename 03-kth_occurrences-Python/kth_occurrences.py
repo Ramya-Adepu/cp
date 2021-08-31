029868981079e6883c09ad541061f5d6d0929bd5
adepu <adepu.ramya@gmail.com>
2021-08-31 23:22:48 +0530

    Submitted

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -5,6 +5,16 @@
 
 
 def fun_kth_occurrences(s, n):
-	return 'a'
+	d = {}
+	for i in s.lower():
+		if i not in d:
+			d[i]=1
+		else:
+			d[i] = d[i]+1
+	l = sorted(d.values(),reverse=True)
+	for key,val in d.items():
+		if val == l[n-1]:
+			return key
+	# return 'a'
 
 
